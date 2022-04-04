a66010f5b343c04f6678f16bfdaf20e5b6d5bf9d
Judist <Judist@workmail.com>
2022-04-04 12:13:21 +0200

    update

--- Websites/resources/views/layouts/sidebar.blade.php
+++ Websites/resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 @if (Route::has('login'))
     <div class="absolute top-0 right-0 mt-sidebar border border-gray-300">
         <h2 class="text-3xl font-semibold text-center text-blue-600">Friend list</h2>
-        <div class="flex flex-col justify-between mt-6">
+        <div class="flex flex-col justify-between mt-4">
             <form action="{{ route('addFriend') }}" method="post">
                 @csrf
                 <div class="items-center">
@@ -12,7 +12,7 @@
                 </div>
             </form>
                 <aside>
-                    <ul></ul>
+                    <ul>
                         <li>
                             <a class="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md " href="#">
 
--- Websites/resources/views/welcome.blade.php
+++ Websites/resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="mt-20 mx-auto center2">
+    <p>Center this text!</p>
+</div>
 @endsection
