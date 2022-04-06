984fa9f8a5cd721644f0c12039664e46f1ace7da
Judist <Judist@workmail.com>
2022-04-06 16:07:31 +0200

    update

--- Websites/app/Models/BlackjackModels/Deck.php
+++ Websites/app/Models/BlackjackModels/Deck.php
@@ -4,6 +4,7 @@ namespace App\Models\BlackjackModels;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\BlackjackModels\Card;
 
 class Deck extends Model
 {
