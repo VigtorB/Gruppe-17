8bec7266e07c3365a0ec5451d6302685d206cd2d
Judist <Judist@workmail.com>
2022-04-21 19:04:37 +0200

    Update

--- Websites/app/Http/Controllers/GamesController.php
+++ Websites/app/Http/Controllers/GamesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class GamesController extends Controller
 {
@@ -13,6 +15,22 @@ class GamesController extends Controller
 
     public function startBlackjack()
     {
-        return view('games.blackjack');
+        $coin_bet = 100;
+        $id = Auth::user()->id;
+        $url = env('API_URL') . 'blackjack/';
+        $ch = curl_init($url);
+        $data = array(
+            'id' => $id,
+            'coin_bet' => $coin_bet,
+        );
+
+        $payload = json_encode($data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        dd($result);
+        //return view('games.blackjack')->with($result);
     }
 }
