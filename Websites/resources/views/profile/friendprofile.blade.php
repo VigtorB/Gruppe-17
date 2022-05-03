7d2f28ff804cbdd6eb3572922fedeb7d8615d6cc
Vigtor <1086219@UCN.dk>
2022-05-03 20:28:05 +0200

    More fucking buttons, jesus fucking christ

--- Websites/resources/views/profile/friendprofile.blade.php
+++ Websites/resources/views/profile/friendprofile.blade.php
@@ -9,20 +9,20 @@
 {{-- add friend button --}}
 
 @if(Auth::user()->id != $user['id'] && $isFriend['isFriend'] == 0)
-<a href="{{ route('addFriend', $user['id']) }}" type="submit" class="btn btn-primary">Add Friend</a>
+<a href="{{ route('addFriend', $user['id']) }}" type="submit" class="btn btn-primary img-blackjack"><img src="/img/buttons/button(add).png"></a>
 
 @endif
 @if(Auth::user()->id != $user['id'] && $isFriend['isFriend'] == 1)
-<a href="{{ route('addFriend', $user['id']) }}" type="submit" class="btn btn-primary">Accept Friend</a>
-<a href="{{ route('deleteFriend', $user['id']) }}" type="submit" class="btn btn-primary">Decline</a>
+<a href="{{ route('addFriend', $user['id']) }}" type="submit" class="btn btn-primary img-blackjack"><img src="/img/buttons/button(accept).png"></a>
+<a href="{{ route('deleteFriend', $user['id']) }}" type="submit" class="btn btn-primary img-blackjack"><img src="/img/buttons/button(decline).png"></a>
 
 @endif
 @if(Auth::user()->id != $user['id'] && $isFriend['isFriend'] == 2)
 Pending friend
-<a href="{{ route('deleteFriend', $user['id']) }}" type="submit" class="btn btn-primary">Cancel request</a>
+<a href="{{ route('deleteFriend', $user['id']) }}" type="submit" class="btn btn-primary img-blackjack"><img src="/img/buttons/button(cancel).png"></a>
 @endif
 @if(Auth::user()->id != $user['id'] && $isFriend['isFriend'] == 3)
-    <a href="{{ route('deleteFriend', $user['id']) }}" type="submit" class="btn btn-primary">Delete Friend</a>
+    <a href="{{ route('deleteFriend', $user['id']) }}" type="submit" class="btn btn-primary img-blackjack"><img src="/img/buttons/button(remove).png"></a>
 @endif
 
 
