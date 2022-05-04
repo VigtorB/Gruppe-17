b15bc9ee799e469f161af8aaee003d15d069a536
Judist <Judist@workmail.com>
2022-05-04 11:41:22 +0200

    Update

--- Websites/public/js/ajax.js
+++ Websites/public/js/ajax.js
@@ -1,5 +1,7 @@
 //TODO: FIX DENNE! Lige nu tilføjer den altid de samme kort på siden. Så den gentager kortene.
 function getGame(value) {
+
+
     console.log(value);
     if (value === "hit") {
         var url = 'http://127.0.0.1:8001/games/blackjack/hit'
@@ -7,29 +9,42 @@ function getGame(value) {
     if (value === "stand") {
         var url = 'http://127.0.0.1:8001/games/blackjack/stand'
     }
-
+    if (value === "newGame") {
+        var url = 'http://127.0.0.1:8001/games/blackjack/startgame'
+    }
     if (value === "startGame") {
         var url = 'http://127.0.0.1:8001/games/blackjack/startgame'
     }
+
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            clearPage();
             data.dealerCard.forEach(dealerCard => document.getElementById('dealer-hand').innerHTML += `<img class="img-cards" src="/img/deck/${dealerCard}.png" class="img-responsive">`);
             data.playerCard.forEach(playerCard => document.getElementById('player-hand').innerHTML += `<img class="img-cards" src="/img/deck/${playerCard}.png" class="img-responsive">`);
             document.getElementById('player-value').innerHTML = data.playerValue;
             document.getElementById('dealer-value').innerHTML = data.dealerValue;
 
+            document.getElementById('newGame').hidden = true;
+            document.getElementById('hit').disabled = false;
+            document.getElementById('hit').hidden = false;
+            document.getElementById('stand').disabled = false;
+            document.getElementById('stand').hidden = false;
+
             if (data.gameStatus != 'pending') {
-                document.getElementsByName('hit').disabled = true;
-                document.getElementsByName('stand').disabled = true;
-                document.getElementsByName('start').innerHTML = `<button type="button" class="btn btn-primary" onclick="playAgain()">Play Again</button>`;
+                document.getElementById('hit').disabled = true;
+                document.getElementById('hit').hidden = true;
+                document.getElementById('stand').disabled = true;
+                document.getElementById('stand').hidden = true;
+                document.getElementById('newGame').hidden = false;
             }
 
             if (data.gameStatus === 'blackjack' || data.gameStatus === 'player win' || data.gameStatus === 'dealer bust') {
                 document.getElementById('game-status').innerHTML = 'You won!';
             }
             if (data.gameStatus === 'pending') {
-                document.getElementById('');
+                document.getElementById('newGame').hidden = true;
             }
             else if (data.gameStatus === 'dealer win' || data.gameStatus === 'bust' || data.gameStatus === 'dealer blackjack') {
                 document.getElementById('game-status').innerHTML = 'You lost!';
@@ -38,6 +53,13 @@ function getGame(value) {
                 document.getElementById('game-status').innerHTML = 'Draw!';
             }
         })
+}
+
+
+
+function clearPage() {
+    document.getElementById('player-hand').innerHTML = '';
+    document.getElementById('dealer-hand').innerHTML = '';
 }
 
 
@@ -54,9 +76,9 @@ function getGame(value) {
     /* data.playerValue.forEach(playerValue => document.getElementById('player-value').innerHTML = data.playerValue);
             data.dealerValue.forEach(dealerValue => document.getElementById('dealer-value').innerHTML = data.dealerValue); */
 
-    /* const json_data = JSON.parse(data);
-    console.log(json_data);
-    } */
+/* const json_data = JSON.parse(data);
+console.log(json_data);
+} */
 
 
 /* function standGame() {
@@ -89,10 +111,9 @@ function getFriends() {
         .then(response => response.json())
         .then(data => data.friend)
         .then(data => {
-            if(data[0] === 'friendless pig')
-            { document.getElementById('friends').innerHTML = 'You have no friends!'; }
+            if (data[0] === 'friendless pig') { document.getElementById('friends').innerHTML = 'You have no friends!'; }
             else {
-            data => data.forEach(friend => document.getElementById('friends').innerHTML += friend);
+                data => data.forEach(friend => document.getElementById('friends').innerHTML += friend);
             }
         })
 }
--- Websites/resources/views/games/blackjack/blackjack.blade.php
+++ Websites/resources/views/games/blackjack/blackjack.blade.php
@@ -1,41 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<script src="/js/ajax.js"></script>
-        <div class="center-block">
-            <script src="/js/ajax.js"></script>
-            <div class="container">
-                <p>Dealer Cards</p>
-                <div id="dealer-hand">
-                    <p id="dealer-value"></p>
-                </div>
+    <script src="/js/ajax.js"></script>
+    <div class="center-block">
+        <script src="/js/ajax.js"></script>
+        <div class="container">
+            <p>Dealer Cards</p>
+            <div id="dealer-hand">
             </div>
+            <p id="dealer-value"></p>
+        </div>
         <div class="container">
             <p>Your Cards</p>
             <div id="player-hand">
-                <p id="player-value"></p>
-
             </div>
+            <p id="player-value"></p>
         </div>
 
-                <div class="column img-blackjack">
+        <div class="column img-blackjack">
 
-                    <button id="game-value" onclick="getGame('hit')"  name="hit"
-                        class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                        <img src="/img/buttons/button(hit).png">
+            <button id="hit" onclick="getGame('hit')" name="hit"
+                class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                <img src="/img/buttons/button(hit).png">
 
-                    </button>
-                    <button id="game-value"  onclick="getGame('stand')" name="stand"
-                        class="ml-4 font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                        <img src="/img/buttons/button(stand).png">
-                    </button>
-                    <button id="game-value" onclick="getGame('startGame')"name = "start">
-                        <img src="/img/buttons/button(playagain).png">
-                    </button>
-                </div>
-            <div>
-
-            </div>
+            </button>
+            <button id="stand" onclick="getGame('stand')" name="stand"
+                class="ml-4 font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                <img src="/img/buttons/button(stand).png">
+            </button>
+            <button id="newGame" onclick="getGame('newGame')" name="start">
+                <img src="/img/buttons/button(playagain).png">
+            </button>
         </div>
-        <script>getGame("startGame");</script>
+        <div>
+
+        </div>
+    </div>
+    <script>
+        getGame("startGame");
+    </script>
 @endsection
