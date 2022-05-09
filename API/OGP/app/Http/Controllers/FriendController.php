2e6e6c7fcd9e75e932e0112b04853a1b1e9a1c7f
Vigtor <1086219@UCN.dk>
2022-05-09 17:26:41 +0200

    Update FriendController.php

--- API/OGP/app/Http/Controllers/FriendController.php
+++ API/OGP/app/Http/Controllers/FriendController.php
@@ -36,54 +36,64 @@ class FriendController extends Controller
     {
         $userController = new UserController();
         try {
+            //db call that constructs array of mutual friends.
             $friend_ids = Friend::where('sender_id', $id)
                 ->where('is_accepted', 1)
                 ->select('receiver_id')
                 ->get();
+            //defines $friends as null if user has no mutual friends.
             if ($friend_ids->isEmpty()) {
                 $friends = null;
-            } else {
+            }
+            //else, define $friends as an array of mutual friends.
+            else {
                 foreach ($friend_ids as $friend_id) {
                     $friend = $friend_id->receiver_id;
                     $friends[] = $userController->getUsername($friend);
                 }
             }
+            //db call that constructs array of friend requests.
             $friend_ids = Friend::where('receiver_id', $id)
                 ->where('is_accepted', 0)
                 ->select('sender_id')
                 ->get();
-
+            //defines $requests as null if user has no friend requests.
             if ($friend_ids->isEmpty()) {
                 $friendRequests = null;
-            } else {
+            }
+            //else, define $requests as an array of friend requests.
+            else {
                 foreach ($friend_ids as $friend_id) {
                     $friend = $friend_id->sender_id;
                     $friendRequests[] = $userController->getUsername($friend);
                 }
             }
+            //constructs a data package able to return multiple variables to the frontend.
             $data = [
                 'friends' => $friends,
                 'friendRequests' => $friendRequests,
             ];
-
-
             return response()->json(['success' => true, 'friend' => $data]);
-        } catch (\Exception $e) {
+        }
+        catch (\Exception $e) {
+            return response()->json(['success' => false]);
+        }
+        /* catch (\Exception $e) {
             $data = [
-                'friends' => ['friendless pig'],
-                'friendRequests' => ['no friend requests you fuck'],
+                'friends' => ['No friends :('],
+                'friendRequests' => ['no friend requests :('],
             ];
             return response()->json(['success' => false, 'friend' => $data]);
-        }
+        } */
     }
 
-    //get friend
+    //get other user
     public function getOtherUser($id, $username)
     {
         try {
             $userController = new UserController();
             $otherUser = $userController->getUser($username);
-            if($id == $otherUser->id){
+            if ($id == $otherUser->id) {
                 return response()->json(['success' => false]);
             }
             $isFriend = $this->isFriend($id, $otherUser->id);
@@ -104,34 +114,38 @@ class FriendController extends Controller
     //is friend
     public function isFriend($id, $friend_id)
     {
-        //make empty arrays
-        /* $friend = [];
-        $friend2 = []; */
-        $friendStatus = [];
-
-        //db call thga
+        //db calls that grabs both users if they exist.
         $friendStatus1 = Friend::where('sender_id', $id)
             ->where('receiver_id', $friend_id)
             ->get();
-
         $friendStatus2 = Friend::where('sender_id', $friend_id)
             ->where('receiver_id', $id)
             ->get();
 
-
-
+        //checking arrays to tell whether users are friends or not,
+        //3 = both are friends
+        //2 = user is friend, other user is not, pending friend
+        //1 = other user is friend, user is not, friend request
+        //0 = neither are friends
         if ($friendStatus1->isEmpty() && $friendStatus2->isEmpty()) {
             return 0;
         }
-        if($friendStatus1->isEmpty() && !$friendStatus2->isEmpty()){
+        if ($friendStatus1->isEmpty() && !$friendStatus2->isEmpty()) {
             return 1;
-        }
-        else{
+        } else {
             return 2;
         }
         return 3;
+    }
 
-        /* $friendStatus = Friend::where(function($query) use($id, $friend_id) {
+    /*
+        //Generating empty arrays
+        $friendStatus1 = [];
+        $friendStatus2 = [];
+        */
+
+    //Attempt at turning DB call into one instead of 2 calls
+    /* $friendStatus = Friend::where(function($query) use($id, $friend_id) {
                 $query->where("sender_id", $id)->where("receiver_id", $friend_id);
               })->andWhere(function($query) use($id, $friend_id) {
                 $query->where("receiver_id", $id)->where("sender_id", $friend_id);
@@ -148,7 +162,6 @@ class FriendController extends Controller
                     }
                 }
                 return 0; */
-    }
 
 
 
